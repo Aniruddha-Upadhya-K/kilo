d418130a9d4daf9c3bc1d52bd15f34548cbb9695
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2025-06-24 23:20:14 +0530

    feat: string slice lib function implementation

--- include/lib.h
+++ include/lib.h
@@ -2,7 +2,10 @@
 #define LIB_H
 
 #include <stdarg.h>
+#include <string.h>
 
 void die(char *s, ...);
+
+void stringSlice(char *s, size_t slen, size_t rlen, int from);
 
 #endif // !LIB_H
--- src/lib.c
+++ src/lib.c
@@ -17,3 +17,25 @@ void die(char *s, ...) {
     perror(err);
     exit(1);
 }
+
+/*
+ * All lengths are coints except last null character
+ */
+void stringSlice(char *s, size_t slen, size_t rlen, int from) {
+    if (rlen >= slen) {
+        s = realloc(s, 1);
+        if (!s) die("In function: %s\r\nAt line: %d\r\nrealloc", __func__, __LINE__);
+        s[0] = '\0';
+        return;
+    }
+
+    if (from > (int) slen - 1) from = slen - 1;
+    if (from < 0) from = 0;
+    if (from + rlen > slen) rlen = slen - from;
+
+    size_t nlen = slen - rlen;
+    memmove(s + from, s + from + rlen, rlen);
+    s = realloc(s, nlen + 1);
+    if (!s) die("In function: %s\r\nAt line: %d\r\nrealloc", __func__, __LINE__);
+    s[nlen] = '\0';
+}
